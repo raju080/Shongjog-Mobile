49dd39fd26508ae71257599544c2f7c88049df7a
AsifAhmed85 <1605085@ugrad.cse.buet.ac.bd>
2021-06-27 07:25:43 +0600

    Tutor Home page Update

--- src/screens/tutor/HomeScreenTutor.tsx
+++ src/screens/tutor/HomeScreenTutor.tsx
@@ -1,26 +1,80 @@
-import * as React from 'react';
-import { StyleSheet, Button, FlatList, View, Text } from 'react-native';
-
-import { Tab } from 'react-native-elements';
-
+import React, {useEffect} from 'react';
+import { StyleSheet, Button, FlatList, ImageBackground, Image, ScrollView } from 'react-native';
 import { useAppSelector, useAppDispatch } from '../../hooks/reactReduxHooks';
-
+import { Text, View } from '../../components/Themed';
+import TutorPreviewCardCol from '../../components/TutorPreviewCardCol';
 import { ReactNavigationProps } from '../../types';
 import { TutorTypeStudent } from '../../types_store';
+import { changeSelectedTutor, fetchTutorsStudent, fetchSelectedTutorStudent, toggleFilterDrawerOpen } from '../../store/actionCreators';
 
-import { changeSelectedTutor } from '../../store/actionCreators';
 
-export default function HomeScreenTutor({ navigation }: ReactNavigationProps) {
+export default function HomeScreenStudent({ navigation }: ReactNavigationProps) {
+	const tutors = useAppSelector(
+		(state): TutorTypeStudent[] => state.tutor.tutors
+	);
+
+	const currentTutor = useAppSelector((state) => state.tutor.selectedTutor);
+	const dispatch = useAppDispatch();
+
+
+	const handleTutorClick = (tutor: TutorTypeStudent) => {
+		console.log("changing selected tutor " + tutor.name)
+		dispatch(fetchSelectedTutorStudent(tutor.userId));
+		navigation.push('TutorDetailsScreenStudent');
+	}
+
+	useEffect(() => {
+		dispatch(fetchTutorsStudent());
+	}, []);
 
 	return (
 		<View style={styles.container}>
-			{/* <Tab>
-				<Tab.Item title='tab 1'>
-					<Text>Tab 1</Text>
-				</Tab.Item>
-				<Tab.Item title='tab 2'></Tab.Item>
-			</Tab> */}
-			<Text style={styles.title}>Tutor Home</Text>
+			<ScrollView>
+				<View style={styles.middleContainer}>
+					<Text style={styles.text1}>My Students</Text>
+					<ScrollView>
+					<View>
+						<View style={{flexDirection: 'row'}}>
+							<Text style={styles.text2}>Current Students</Text>
+						</View>
+						<FlatList
+							horizontal
+							data = {tutors}
+							renderItem = {({item}) => (
+								<TutorPreviewCardCol tutor={item} onClick={handleTutorClick} />
+							)
+						}
+						/>
+					</View>
+					<View>
+						<View style={{flexDirection: 'row'}}>
+							<Text style={styles.text2}>Requested Students</Text>
+						</View>
+						<FlatList
+							horizontal
+							data = {tutors}
+							renderItem = {({item}) => (
+								<TutorPreviewCardCol tutor={item} onClick={handleTutorClick} />
+							)
+						}
+						/>
+					</View>
+					<View>
+						<View style={{flexDirection: 'row'}}>
+							<Text style={styles.text2}>Previous Students</Text>
+						</View>
+						<FlatList
+							horizontal
+							data = {tutors}
+							renderItem = {({item}) => (
+								<TutorPreviewCardCol tutor={item} onClick={handleTutorClick} />
+							)
+						}
+						/>
+					</View>
+					</ScrollView>
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
@@ -28,8 +82,8 @@ export default function HomeScreenTutor({ navigation }: ReactNavigationProps) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
+		flexDirection: 'column',
+		justifyContent: 'flex-start',
 	},
 	title: {
 		fontSize: 20,
@@ -39,5 +93,33 @@ const styles = StyleSheet.create({
 		marginVertical: 30,
 		height: 1,
 		width: '80%',
+	},
+	upperContainer: {
+		alignItems: 'center',
+		paddingBottom: 10
+	},
+	middleContainer: {
+		alignItems: 'flex-start',
+		paddingTop: 10,
+		paddingLeft: 10
+	},
+	text1: {
+		marginTop: 10,
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	text2: {
+		marginTop: 10,
+		fontSize: 16,
+	},
+	text3: {
+		marginTop: 10,
+		fontSize: 16,
+		color: '#B55A30',
+		alignSelf: 'flex-end',
+		paddingRight: 10
+	},
+	searchMore: {
+		flex: 1,
 	},
 });
