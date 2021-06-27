8895f9b530c5ce9ba99547d00bbcaea330478c50
AsifAhmed85 <1605085@ugrad.cse.buet.ac.bd>
2021-06-27 07:14:08 +0600

    Tutor HomeScreen Update

--- src/shared/baseURL.tsx
+++ src/shared/baseURL.tsx
@@ -2,7 +2,7 @@
 // export const baseURL = 'http://192.168.31.178:3000/api';
 
 // raju
-export const baseURL = 'http://192.168.43.98:3000/api';
+// export const baseURL = 'http://192.168.43.98:3000/api';
 
 // asif
-// export const baseURL = 'http://192.168.31.20:3000/api';
+export const baseURL = 'http://192.168.31.20:3000/api';
